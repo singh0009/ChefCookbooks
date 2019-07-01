e8f89519d62522cf786b20bf835b6bd4dc664b21
root <singh.babber@gmail.com>
2019-07-01 14:59:50 -0400

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,19 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp' do
+	action :install
+end
+
+package 'git' do
+	action :install
+end
+
+
+file '/etc/motd' do
+	content 'This is a test file'
+	owner 'osboxes'
+	group 'root'
+	mode '0644'
+end
